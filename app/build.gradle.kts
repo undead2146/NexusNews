6b95e6b68e6555332e67d57765b8031eeff6e44d
undead2146 <mustafa2146@live.be>
2026-01-16 10:00:04 +0100

    chore(deps): upgrade Room and add icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.retrofit)
 
     // MVVM + Compose Extra
